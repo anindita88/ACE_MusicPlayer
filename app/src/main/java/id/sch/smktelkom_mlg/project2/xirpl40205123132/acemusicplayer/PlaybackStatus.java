8888ccbb5c6e73cb585e92eca3aa78a9a2a5d855
Anindita <anindita.ekasuci88@gmail.com>
2017-04-01 10:34:23 +0700

    PlaySong

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40205123132/acemusicplayer/PlaybackStatus.java
@@ -0,0 +1,10 @@
+package id.sch.smktelkom_mlg.project2.xirpl40205123132.acemusicplayer;
+
+/**
+ * Created by Ainnaya on 30/03/2017.
+ */
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40205123132/acemusicplayer/onItemClickListener.java
@@ -0,0 +1,11 @@
+package id.sch.smktelkom_mlg.project2.xirpl40205123132.acemusicplayer;
+
+import android.view.View;
+
+/**
+ * Created by Ainnaya on 30/03/2017.
+ */
+
+public interface onItemClickListener {
+    void onClick(View view, int index);
+}
